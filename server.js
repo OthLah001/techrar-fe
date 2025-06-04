c2d3bf002de45cbce3e02f674af0050511b9b694
OthmaneLh001 <othmane.lahrimi.work@gmail.com>
2025-06-04 22:16:31 +0000

    fix: fix project dist path & add path lib

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(__dirname + '/dist/project-gutenberg-fe/browser'));
+app.use(express.static(__dirname + '/dist/techrar-fe/browser'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/project-gutenberg-fe/browser/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/techrar-fe/browser/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
